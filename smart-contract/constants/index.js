8c8ee51bb52a1a30b14d0d7a0abb3a8e27594169
Sunthosh <heysunthosh@gmail.com>
2022-02-05 12:10:31 +0530

    Added code for ICO-Dapp

---
+++ smart-contract/constants/index.js
@@ -0,0 +1,5 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS =
+  "0xaC1c085bb86Fc38732882561CBc1f0Df22A1F477";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
